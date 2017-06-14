035cf8f0bd98a053eaa48e6ceab02c0000da9474
XIEKE <xie_ke_ok@126.com>
2017-06-14 21:48:43 +0800

    spider to jiandan
    get ooxx from jiandan

---
+++ ooxx.py
@@ -0,0 +1,36 @@
+#author __keal__
+#coding: utf-8
+
+import requests
+from bs4 import BeautifulSoup
+
+index = 0
+headers = {'referer': 'http://jandan.net/', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+
+
+# 在当前目录下保存图片
+def save_jpg(res_url):
+    global index
+    oo_num=[]
+    n=0
+    html = BeautifulSoup(requests.get(res_url).text, 'html.parser')
+#判断需要oo大于多少的数
+    for oo in html.find_all('div',class_='jandan-vote'):
+        oo_num.append(int(oo.find('span').string))
+    print(oo_num)
+    for link in html.find_all('a', {'class': 'view_img_link'}):
+        if oo_num[n]>150:
+            with open('{}.{}'.format(index, link.get('href')[len(link.get('href'))-3: len(link.get('href'))]), 'wb') as jpg:
+                jpg.write(requests.get("http:" + link.get('href')).content)
+                print("正在抓取第%s条数据" % index)
+                index += 1
+        if n<len(oo_num)-1:
+           n+=1
+
+
+#  抓取煎蛋妹子图片，根据range的范围抓取前多少页
+if __name__ == '__main__':
+    url = 'http://jandan.net/ooxx'
+    for i in range(0, 108):
+        save_jpg(url)
+        url = BeautifulSoup(requests.get(url).text,'html.parser').find('a', {'class': 'previous-comment-page'}).get('href')
